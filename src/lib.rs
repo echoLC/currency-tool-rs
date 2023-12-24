7f3e3323c636444fe15c73dc1dd3fb5d3b133dd4
luochao <luochao@joyy.com>
2023-12-24 13:19:38 +0800

    test: add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,17 @@ mod default_config;
 
 pub use crate::default_config::*;
 
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn default_currency () {
+    assert_eq!(get_default_currency(), "USD");
+  }
+
+  #[test]
+  fn default_rate () {
+    assert_eq!(get_default_rate(), 0.7);
+  }
+}
