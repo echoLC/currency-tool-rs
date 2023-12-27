ad25d602ed00149b2c40cef697f127c1b2e836f8
luochao <luochao@joyy.com>
2023-12-27 15:43:25 +0800

    feat: 添加货币 format 方法以及测试用例

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,35 @@ mod default_config;
 
 pub use crate::default_config::*;
 
+fn format_number_by_group_symbol(num: f32, symbol: String) -> String {
+  String::from("5.2")
+}
+
+pub struct CurrencyRate {
+  usd: f32,
+  cny: f32,
+  my: f32,
+  jpy: f32
+}
+
+pub struct FormatOption {
+  from: String,
+  to: String,
+  currency_rates: CurrencyRate
+}
+
+fn basic_format(value: f32, with_currency: bool, option: FormatOption) -> String {
+  String::from("5.2")
+}
+
+pub fn format(value: f32, option: FormatOption) -> String {
+  basic_format(value, true, option)
+}
+
+pub fn format_without_currency(value: f32, option: FormatOption) -> String {
+  basic_format(value, false, option)
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
@@ -29,5 +58,19 @@ mod test {
   #[test]
   fn default_format_symbol() {
     assert_eq!(get_default_format_symbol(), "amount");
+  }
+
+  #[test]
+  fn basic_format() {
+    assert_eq!(format(100.0, FormatOption {
+      from: String::from("USD"),
+      to: String::from("EUR"),
+      currency_rates: CurrencyRate{
+        usd: 0.7,
+        jpy: 0.4,
+        my: 0.2,
+        cny: 1.0
+      }
+    }), "€0,92 EUR");
   }
 }
