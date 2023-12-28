8d56c6fa43c5b8d2c5a537c195b4ae3d8b4701b1
luochao <luochao@joyy.com>
2023-12-28 17:26:24 +0800

    test: 增加 test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use currency_tool_rs::{get_default_currency, get_default_rate, get_frac};
+use currency_tool_rs::{get_default_currency, get_default_rate};
 
 fn main() {
     println!("default currency:{}", get_default_currency());
     println!("default currency rate:{}", get_default_rate());
-    println!("{}", get_frac(1.212));
-    println!("{}", get_frac(3.1412));
-    println!("{}", get_frac(3.000));
 }
