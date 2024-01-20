48374e05514c50c4555b631bc7862e98b760cf3c
luochao <luochao@joyy.com>
2024-01-20 18:12:49 +0800

    feat: 增加获取汇率逻辑

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,25 @@
 mod default_config;
 
+
 pub use crate::default_config::*;
 
+#[derive(Debug, Clone)]
 pub struct CurrencyRates {
   usd: f64,
-  gbp: f64
+  gbp: f64,
+  cny: f64
 }
 
+#[derive(Debug, Clone)]
 pub struct CommonFormatOption {
   from: String,
   to: String,
   currency_rates: CurrencyRates
+}
+
+pub struct ConvertRates {
+  to: f64,
+  from: f64
 }
 
 fn pretty_print_with_symbol (value: &str, symbol: char) -> String {
@@ -19,6 +28,7 @@ fn pretty_print_with_symbol (value: &str, symbol: char) -> String {
   let a = value.chars().rev().enumerate();
 
   for(index, val) in a {
+
     if index != 0 && index % 3 == 0 {
       s.insert(0, symbol);
     }
@@ -38,12 +48,38 @@ fn get_currency_symbol (currency: Option<&str>) -> String {
   }
 }
 
+fn get_convert_rates(from_currency: &str, to_currency: &str, rates: CurrencyRates) -> ConvertRates  {
+
+  let mut to_rate: f64 = 1.0;
+  let mut from_rate: f64 = 1.0;
+
+  if from_currency == "USD" {
+    from_rate = rates.usd;
+  } else if from_currency == "GBP" {
+    from_rate = rates.gbp;
+  } else if from_currency == "CNY" {
+    from_rate = rates.cny;
+  }
+
+  if to_currency == "USD" {
+    to_rate = rates.usd;
+  } else if to_currency == "GBP" {
+    to_rate = rates.gbp;
+  } else if to_currency == "CNY" {
+    to_rate = rates.cny;
+  }
+
+  ConvertRates{
+    to: to_rate,
+    from: from_rate
+  }
+}
+
 pub fn format(value: f64, options: CommonFormatOption) -> String {
   let to = options.to;
-  let currency_rates = options.currency_rates;
-  let from_rate = currency_rates.usd;
-  let to_rate = currency_rates.gbp;
-  let convert_value = value * to_rate / from_rate;
+  let currency_rates = get_convert_rates(&options.from, &to, options.currency_rates);
+
+  let convert_value = value * currency_rates.to / currency_rates.from;
   let convert_value_str = format!("{:.2}", convert_value);
   let convert_split_arr: Vec<&str> = convert_value_str.split(".").collect();
   let convert_int_part = convert_split_arr[0];
@@ -92,6 +128,7 @@ mod test {
       currency_rates: CurrencyRates{
         usd: 1.0,
         gbp: 0.808686,
+        cny: 0.140449
       }
     }), "£80.87 GBP");
   }
@@ -104,6 +141,7 @@ mod test {
       currency_rates: CurrencyRates{
         usd: 1.0,
         gbp: 0.808686,
+        cny: 0.140449
       }
     }), "£177,854.31 GBP");
   }
@@ -116,7 +154,21 @@ mod test {
       currency_rates: CurrencyRates{
         usd: 1.0,
         gbp: 0.808686,
+        cny: 0.140449
       }
     }), "￥177,854.31 CNY");
+  }
+
+  #[test]
+  fn format_different_rates() {
+    assert_eq!(format(219930.00, CommonFormatOption {
+      from: String::from("USD"),
+      to: String::from("CNY"),
+      currency_rates: CurrencyRates{
+        usd: 1.0,
+        gbp: 0.808686,
+        cny: 0.140449
+      }
+    }), "￥30,888.95 CNY");
   }
 }
