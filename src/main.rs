910da617afc3184481279cca4893cfb1d3b49007
luochao <luochao@joyy.com>
2024-01-23 10:00:06 +0800

    feat: 添加 lifetime demo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use currency_tool_rs::{get_default_currency, get_default_rate, SymbolOrderEnum};
+use currency_tool_rs::{get_default_currency, get_default_rate};
 
 fn main() {
     println!("default currency:{}", get_default_currency());
     println!("default currency rate:{}", get_default_rate());
-    println!("{:.2}", 12.0);
+    
+    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
 
-    let prefix = SymbolOrderEnum::Prefix;
+    let string1 = String::from("hello");
 
-    dbg!(prefix);
+    {
+        let string2 = String::from("world");
+        let result = longest(&string1, &string2);
+
+        println!("result is {}", result);
+    }
 }
