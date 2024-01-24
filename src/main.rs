b5644f25ac8de0ea6e5e8188adceacb975b2a75b
luochao <luochao@joyy.com>
2024-01-24 10:13:08 +0800

    feat: 一个复杂的生命周期演示的例子

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use currency_tool_rs::{get_default_currency, get_default_rate};
+use std::fmt::Display;
 
 fn main() {
     println!("default currency:{}", get_default_currency());
     println!("default currency rate:{}", get_default_rate());
     
-    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str where T: Display {
+        println!("Announcement! {}", ann);
+
         if x.len() > y.len() {
             x
         } else {
@@ -16,7 +19,7 @@ fn main() {
 
     {
         let string2 = String::from("world");
-        let result = longest(&string1, &string2);
+        let result = longest_with_an_announcement(&string1, &string2, String::from("hello"));
 
         println!("result is {}", result);
     }
