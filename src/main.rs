68ac13d97c99fbb4e622d3ad51eeab7dcd6df5eb
luochao <luochao@joyy.com>
2024-03-23 10:16:24 +0800

    feature: finish the 'factors' exercise

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,103 @@
-fn is_prime (num: u32) -> bool {
-    if num <= 1 {
-        return false;
+pub fn factors(n: u64) -> Vec<u64> {
+  let mut res: Vec<u64> = Vec::new();
+  let mut divisor: u64 = 2;
+  let mut original_value = n;
+
+  while original_value >= 2 {
+    if original_value % divisor == 0 {
+      res.push(divisor);
+
+      original_value = original_value / divisor;
+    } else {
+      divisor += 1;
     }
+  }
 
-    let mut i = 2;
-
-    while i <= (num as f64).sqrt() as u32 {
-        if num % i == 0 {
-            return false;
-        }
-
-        i += 1;
-    }
-
-    true
+  res
 }
 
-pub fn nth(n: u32) -> u32 {
-    let mut index = 0;
-    let mut num = 2;
-
-    while index <= n {
-        if index == n {
-            return num;
-        } else {
-            num += 1;
-        }
-
-        if is_prime(num) {
-            index += 1;
-        }
-    }
-
-    num
-}
 
 fn main() {
-   nth(1);
+
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn first_prime() {
-        assert_eq!(nth(0), 2);
-    }
-    #[test]
-    fn second_prime() {
-        assert_eq!(nth(1), 3);
-    }
-    #[test]
-    fn sixth_prime() {
-        assert_eq!(nth(5), 13);
-    }
-    #[test]
-    fn big_prime() {
-        assert_eq!(nth(10_000), 104_743);
-    }
+  #[test]
+  fn no_factors() {
+      let factors = factors(1);
+      let expected = [];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn prime_number() {
+      let factors = factors(2);
+      let expected = [2];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn another_prime_number() {
+      let factors = factors(3);
+      let expected = [3];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn square_of_a_prime() {
+      let factors = factors(9);
+      let expected = [3, 3];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn product_of_first_prime() {
+      let factors = factors(4);
+      let expected = [2, 2];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn cube_of_a_prime() {
+      let factors = factors(8);
+      let expected = [2, 2, 2];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn product_of_second_prime() {
+      let factors = factors(27);
+      let expected = [3, 3, 3];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn product_of_third_prime() {
+      let factors = factors(625);
+      let expected = [5, 5, 5, 5];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn product_of_first_and_second_prime() {
+      let factors = factors(6);
+      let expected = [2, 3];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn product_of_primes_and_non_primes() {
+      let factors = factors(12);
+      let expected = [2, 2, 3];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn product_of_primes() {
+      let factors = factors(901255);
+      let expected = [5, 17, 23, 461];
+      assert_eq!(factors, expected);
+  }
+  #[test]
+  fn factors_include_a_large_prime() {
+      let factors = factors(93819012551);
+      let expected = [11, 9539, 894119];
+      assert_eq!(factors, expected);
+  }
 
 }
 
