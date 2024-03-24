078234b181729b0637bc6f0d9d87d13e00c5041a
luochao <luochao@joyy.com>
2024-03-24 09:08:23 +0800

    feat: add 'sum_of_multiples' exercise

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
-pub fn build_proverb(list: &[&str]) -> String {
-  let mut res: Vec<String> = Vec::new();
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+  let mut res: Vec<u32> = Vec::new();
 
-  for i in 0..list.len() {
-    // 最后一项
-    if i == list.len() - 1 {
-      let proverb_line = String::new() + "And all for the want of a " + list.first().unwrap() + ".";
-
-      res.push(proverb_line);
-
-      break;
-    }
-
-    if !list[ i + 1].is_empty() {
-      let proverb_line = String::new() + "For want of a " + list[i] + " the " + list[ i + 1] +  " was lost.";
-
-      res.push(proverb_line);
-    }
+  if limit < 3 {
+    return 0;
   }
 
-  res.join("\n")
+  if factors.len() == 0 {
+    return 0;
+  }
+
+  let mut is_repeated: bool = false;
+  let start = *factors.first().unwrap();
+
+  for i in start..limit {
+    for factor in factors {
+      if *factor == 0 {
+        continue;
+      }
+      if i % factor == 0 && !is_repeated {
+        res.push(i);
+  
+        is_repeated = true;
+      }
+    }  
+
+    is_repeated = false;
+  }
+
+  res.iter().sum()
 }
 
 
 fn main() {
-  let input = &["nail", "shoe"];
-  let output = build_proverb(input);
+  let factors = &[3];
+  let limit = 7;
+  let output = sum_of_multiples(limit, factors);
   println!("{}", output);
 }
 
@@ -34,73 +44,134 @@ mod tests {
   use super::*;
 
   #[test]
-  fn zero_pieces() {
-      let input = &[];
-      let output = build_proverb(input);
-      let expected = String::new();
-      assert_eq!(output, expected);
-  }
-  #[test]
-  fn one_piece() {
-      let input = &["nail"];
-      let output = build_proverb(input);
-      let expected: String = ["And all for the want of a nail."].join("\n");
-      assert_eq!(output, expected);
-  }
-  #[test]
-  fn two_pieces() {
-      let input = &["nail", "shoe"];
-      let output = build_proverb(input);
-      let expected: String = [
-          "For want of a nail the shoe was lost.",
-          "And all for the want of a nail.",
-      ]
-      .join("\n");
-      assert_eq!(output, expected);
-  }
-  #[test]
-  fn three_pieces() {
-      let input = &["nail", "shoe", "horse"];
-      let output = build_proverb(input);
-      let expected: String = [
-          "For want of a nail the shoe was lost.",
-          "For want of a shoe the horse was lost.",
-          "And all for the want of a nail.",
-      ]
-      .join("\n");
-      assert_eq!(output, expected);
-  }
-  #[test]
-  fn full_proverb() {
-      let input = &[
-          "nail", "shoe", "horse", "rider", "message", "battle", "kingdom",
-      ];
-      let output = build_proverb(input);
-      let expected: String = [
-          "For want of a nail the shoe was lost.",
-          "For want of a shoe the horse was lost.",
-          "For want of a horse the rider was lost.",
-          "For want of a rider the message was lost.",
-          "For want of a message the battle was lost.",
-          "For want of a battle the kingdom was lost.",
-          "And all for the want of a nail.",
-      ]
-      .join("\n");
-      assert_eq!(output, expected);
-  }
-  #[test]
-  fn four_pieces_modernized() {
-      let input = &["pin", "gun", "soldier", "battle"];
-      let output = build_proverb(input);
-      let expected: String = [
-          "For want of a pin the gun was lost.",
-          "For want of a gun the soldier was lost.",
-          "For want of a soldier the battle was lost.",
-          "And all for the want of a pin.",
-      ]
-      .join("\n");
-      assert_eq!(output, expected);
-  }
+fn no_multiples_within_limit() {
+    let factors = &[3, 5];
+    let limit = 1;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 0;
+    assert_eq!(output, expected);
+}
+#[test]
+fn one_factor_has_multiples_within_limit() {
+    let factors = &[3, 5];
+    let limit = 4;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 3;
+    assert_eq!(output, expected);
+}
+#[test]
+fn more_than_one_multiple_within_limit() {
+    let factors = &[3];
+    let limit = 7;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 9;
+    assert_eq!(output, expected);
+}
+#[test]
+fn more_than_one_factor_with_multiples_within_limit() {
+    let factors = &[3, 5];
+    let limit = 10;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 23;
+    assert_eq!(output, expected);
+}
+#[test]
+fn each_multiple_is_only_counted_once() {
+    let factors = &[3, 5];
+    let limit = 100;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 2318;
+    assert_eq!(output, expected);
+}
+#[test]
+fn a_much_larger_limit() {
+    let factors = &[3, 5];
+    let limit = 1000;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 233168;
+    assert_eq!(output, expected);
+}
+#[test]
+fn three_factors() {
+    let factors = &[7, 13, 17];
+    let limit = 20;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 51;
+    assert_eq!(output, expected);
+}
+#[test]
+fn factors_not_relatively_prime() {
+    let factors = &[4, 6];
+    let limit = 15;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 30;
+    assert_eq!(output, expected);
+}
+#[test]
+fn some_pairs_of_factors_relatively_prime_and_some_not() {
+    let factors = &[5, 6, 8];
+    let limit = 150;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 4419;
+    assert_eq!(output, expected);
+}
+#[test]
+fn one_factor_is_a_multiple_of_another() {
+    let factors = &[5, 25];
+    let limit = 51;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 275;
+    assert_eq!(output, expected);
+}
+#[test]
+fn much_larger_factors() {
+    let factors = &[43, 47];
+    let limit = 10000;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 2203160;
+    assert_eq!(output, expected);
+}
+#[test]
+fn all_numbers_are_multiples_of_1() {
+    let factors = &[1];
+    let limit = 100;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 4950;
+    assert_eq!(output, expected);
+}
+#[test]
+fn no_factors_means_an_empty_sum() {
+    let factors = &[];
+    let limit = 10000;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 0;
+    assert_eq!(output, expected);
+}
+#[test]
+fn the_only_multiple_of_0_is_0() {
+    let factors = &[0];
+    let limit = 1;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 0;
+    assert_eq!(output, expected);
+}
+#[test]
+fn the_factor_0_does_not_affect_the_sum_of_multiples_of_other_factors() {
+    let factors = &[3, 0];
+    let limit = 4;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 3;
+    assert_eq!(output, expected);
+}
+#[test]
+fn solutions_using_include_exclude_must_extend_to_cardinality_greater_than_3() {
+    let factors = &[2, 3, 5, 7, 11];
+    let limit = 10000;
+    let output = sum_of_multiples(limit, factors);
+    let expected = 39614537;
+    assert_eq!(output, expected);
+}
+
   
 
 }
